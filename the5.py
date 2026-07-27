1b3eca9f0e80b51fdddb358aad254df4eacb9c21
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-27 14:55:22 -0700

    Update the5.py

--- the5.py
+++ the5.py
@@ -38,5 +38,5 @@ if st.button("السؤال التالي "):
     del st.session_state.sign
     st.rerun()
 if st.session_state.num==10:
-  st.succes("لقد اجتزت هذا الليفل ")
+  st.success("لقد اجتزت هذا الليفل ")
 st.write("your points are " ,st.session_state.num,"from",st.session_state.count,"Questions")
