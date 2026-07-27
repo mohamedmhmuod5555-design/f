4a7a0eb79b30d9db762cbc1b0b4e3a92a74c38a2
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-27 15:59:56 -0700

    Update the5.py

--- the5.py
+++ the5.py
@@ -47,4 +47,6 @@ if st.session_state.num==1:
      del st.session_state.num1
      del st.session_state.num2
      del st.session_state.sign
+else:
+   st.session_state.ran=20
 st.write("your points are " ,st.session_state.num,"from",st.session_state.count,"Questions")
