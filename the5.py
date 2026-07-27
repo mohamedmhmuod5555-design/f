64ba78ef2606ddfdfce3291c7f3efcdb31de957e
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-27 15:34:11 -0700

    Update the5.py

--- the5.py
+++ the5.py
@@ -5,7 +5,7 @@ if 'button_text' not in st.session_state:
 if 'num' not in st.session_state:
   st.session_state.num=0
 if 'sc' not in st.session_state:
-  st.session_state.sc
+  st.session_state.sc=0
 if 'count' not in st.session_state:
   st.session_state.count=0
 if 'num1' not in st.session_state:
