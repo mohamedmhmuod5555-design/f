e07ca2bc1e0c036adf91b83af86e16a781d438e1
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-27 14:49:29 -0700

    Update the5.py

--- the5.py
+++ the5.py
@@ -26,7 +26,7 @@ st.write(num1,sign,num2)
 number=st.number_input("ادخل النتيجه ")
 if st.button("تأكيد التخمين "):
   st.session_state.count += 1
- if number == sc:
+  if number == sc:
     st.success("اجابتك صحيحه ! لقد احسنت")
     st.session_state.num += 1
 else:
